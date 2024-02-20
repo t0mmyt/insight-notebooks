2a6271f54af99ceeeddd3b1351c69b44fec05af9
Tom Taylor <tom@tom.dev>
2024-02-20 16:02:07 +1100

    wip

--- insight/util/metrics.py
+++ insight/util/metrics.py
@@ -12,6 +12,6 @@ def rms(x: np.array):
 
 def snr(arrival: UTCDateTime, window_s: float, data: Trace):
     """Calculate SNR based on RMS either side of arrival"""
-    slice_1 = data.slice(arrival - timedelta(seconds=window_s), arrival)
-    slice_2 = data.slice(arrival, arrival + timedelta(seconds=window_s))
-    return 10 * math.log10(rms(slice_2.data) / rms(slice_1.data))
+    noise = data.slice(arrival - timedelta(seconds=window_s), arrival)
+    signal = data.slice(arrival, arrival + timedelta(seconds=window_s))
+    return 10 * math.log10(rms(signal.data) / rms(noise.data))
